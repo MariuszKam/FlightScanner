162f83fa919c11c4a2ab346782640aa70c18604d
=Talha Kumcu <=mkumcu151@gmail.com>
2024-01-28 12:16:30 +0100

    SQL Script

--- flight_schema.sql
+++ flight_schema.sql
@@ -2,28 +2,29 @@ CREATE SCHEMA IF NOT EXISTS flightScanner;
 USE flightScanner;
 
 CREATE TABLE IF NOT EXISTS Airport(
-airport_id BIGINT PRIMARY KEY NOT NULL ,
-airport_name VARCHAR(255) ,
-x_latitude DOUBLE ,
-y_longitude DOUBLE 
+id BIGINT PRIMARY KEY NOT NULL ,
+name VARCHAR(255) ,
+latitude DOUBLE ,
+longitude DOUBLE 
 );
 select * from airport;
 
 CREATE TABLE IF NOT EXISTS Airline(
-airline_id BIGINT PRIMARY KEY NOT NULL ,
-airline_name VARCHAR(255)
+id BIGINT PRIMARY KEY NOT NULL ,
+name VARCHAR(255)
 );
 select * from airline;
 
 CREATE TABLE IF NOT EXISTS Flight(
-flight_id BIGINT PRIMARY KEY NOT NULL,
-flight_name VARCHAR(255),
-flight_airline_id BIGINT,
-flight_price DOUBLE ,
-start_airport_id BIGINT   ,
-destination_airport_id BIGINT ,
-FOREIGN KEY (start_airport_id) REFERENCES Airport(airport_id),
-FOREIGN KEY (destination_airport_id) REFERENCES Airport(airport_id),
-FOREIGN KEY (flight_airline_id) REFERENCES Airline(airline_id)
+id BIGINT PRIMARY KEY NOT NULL,
+name VARCHAR(255),
+start_id BIGINT   ,
+destination_id BIGINT ,
+airline_id BIGINT,
+price DOUBLE ,
+FOREIGN KEY (start_id) REFERENCES Airport(id),
+FOREIGN KEY (destination_id) REFERENCES Airport(id),
+FOREIGN KEY (airline_id) REFERENCES Airline(id)
 );
 select * from flight;
+show tables;
