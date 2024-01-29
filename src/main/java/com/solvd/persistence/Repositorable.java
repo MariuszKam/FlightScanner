dcdd85a834fdf7f189026f8759761a3b1200d69b
Mariusz <rexxarhell@gmail.com>
2024-01-29 09:49:59 +0100

    initial setup of mybatis connection

---
+++ src/main/java/com/solvd/persistence/Repositorable.java
@@ -0,0 +1,12 @@
+package com.solvd.persistence;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repositorable<T> {
+    void create(T t); //Create
+    Optional<T> loadById(Long id); //Read
+
+    List<T> loadAll();
+
+}
