3fae3837957bc1bed173a22134fbadfa7adfedaf
Mariusz <rexxarhell@gmail.com>
2024-01-30 11:17:04 +0100

    Devided interfaces 'cuz of MyBaits

---
+++ src/main/java/com/solvd/persistence/interfaces/AirlineRepository.java
@@ -0,0 +1,7 @@
+package com.solvd.persistence.interfaces;
+
+import com.solvd.model.Airline;
+import com.solvd.persistence.utility.Repositorable;
+
+public interface AirlineRepository extends Repositorable<Airline> {
+}
