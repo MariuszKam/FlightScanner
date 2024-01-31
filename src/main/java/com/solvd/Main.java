8544a928235a73c9c100b395c7369b1ab36b60a4
Pawel-12 <122164162+Pawel-12@users.noreply.github.com>
2024-01-31 20:55:17 +0100

    Merge Main.java

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -13,6 +13,43 @@ public class Main {
     public static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        /*  FlightService flightService = new FlightService();
+        AirportService airportService= new AirportService();
+        AirlineService airlineService = new AirlineService();
+
+
+        Map<String, List<String>> groupedFlightsInfo = flightService.getDetailedFlightsInfoGroupedByCity();
+
+
+        for (Map.Entry<String, List<String>> entry : groupedFlightsInfo.entrySet()) {
+            System.out.println("City: " + entry.getKey());
+            for (String flightInfo : entry.getValue()) {
+                System.out.println(" - " + flightInfo);
+            }
+        }
+        airportService.getAll().forEach(System.out::println);
+
+        System.out.println(airportService.getById(2L));
+        flightService.getAll().forEach(System.out::println);
+
+        // Initialize with proper values
+        Airport startAirport = new Airport(100L,"Warsaw",324d,324d);
+        Airport destinationAirport = new Airport(101L,"New York",353d,863d);
+        Airline airline = new Airline(100L,"Air");
+        Flight newFlight = new Flight(100L,"name",startAirport,destinationAirport,airline,503d);
+        airportService.creates(startAirport);
+        airportService.creates(destinationAirport);
+        airlineService.creates(airline);
+        flightService.creates(newFlight);
+
+
+        Long flightId = 1L;
+        Optional<Flight> flightOptional = flightService.getById(flightId);
+        flightOptional.ifPresent(flight -> System.out.println("Flight found: " + flight.getName()));
+
+
+        flightService.getAll().forEach(flight -> System.out.println("Flight: " + flight.getName()));*/
+
         Airport a1 = new Airport("New York Airport", 40.7167, -74.0);
         Airport a2 = new Airport("Lisbona Airport", 38.7072, -9.1355);
         Airport a3 = new Airport("Madrid Airport", 40.4167, -3.7033);
