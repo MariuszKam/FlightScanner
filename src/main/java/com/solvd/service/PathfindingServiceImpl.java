7567144fd124023caac6511faeaaf89e1e9c4933
Your Name <you@example.com>
2024-02-01 22:52:51 +0100

    removed service injection

--- src/main/java/com/solvd/service/PathfindingServiceImpl.java
+++ src/main/java/com/solvd/service/PathfindingServiceImpl.java
@@ -1,10 +1,9 @@
-package com.solvd.service.impl;
+package com.solvd.service;
 
 import com.solvd.model.Airport;
 import com.solvd.model.Flight;
-import com.solvd.service.FlightService;
-import com.solvd.service.PathfindingService;
-import com.solvd.service.impl.utility.EarthDistanceCalculator;
+import com.solvd.service.serviceinterface.PathfindingService;
+import com.solvd.service.utility.EarthDistanceCalculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,8 +19,8 @@ public class PathfindingServiceImpl implements PathfindingService {
 
     private final FlightService flightService;
 
-    public PathfindingServiceImpl(FlightService flightService) {
-        this.flightService = flightService;
+    public PathfindingServiceImpl() {
+        this.flightService = new FlightService();
     }
 
     @Override
