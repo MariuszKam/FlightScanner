322ffaa7b59abdd658894b7b00e4bbb050f4b1ea
Mariusz <rexxarhell@gmail.com>
2024-02-01 20:11:23 +0100

    Added Lombok to pom

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-
+        FlightScanner.printAirports();
     }
 }
