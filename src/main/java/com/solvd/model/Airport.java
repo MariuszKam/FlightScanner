d3e36b22cbf0de2f43ad3a50a815d46ebbfc2637
Mariusz <rexxarhell@gmail.com>
2024-02-01 20:24:04 +0100

    Implemented Lombok

--- src/main/java/com/solvd/model/Airport.java
+++ src/main/java/com/solvd/model/Airport.java
@@ -1,83 +1,17 @@
 package com.solvd.model;
 
-import java.util.Objects;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Airport {
     private Long id;
     private String name;
     private Double latitude;
     private Double longitude;
 
-    public Airport(String name, Double latitude, Double longitude) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Airport(Long id, String name, Double latitude, Double longitude) {
-        this.id = id;
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    @Override
-    public String toString() {
-        return '"' + name + "\" airport, " +
-                "id = " + id + ", [X,Y] = [" + latitude + ',' + longitude + ']';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Airport airport = (Airport) o;
-
-        if (!Objects.equals(id, airport.id)) return false;
-        if (!Objects.equals(name, airport.name)) return false;
-        if (!Objects.equals(latitude, airport.latitude)) return false;
-        return Objects.equals(longitude, airport.longitude);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (latitude != null ? latitude.hashCode() : 0);
-        result = 31 * result + (longitude != null ? longitude.hashCode() : 0);
-        return result;
-    }
 }
